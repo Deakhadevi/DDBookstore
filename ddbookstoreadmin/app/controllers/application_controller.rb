a81ee0a81d8c324f8f2acf57411c12a63897a754
Deakhadevi <psdurgadevi@gmail.com>
2023-01-18 19:59:16 +0530

    Durgadevi|Added user login details

---
+++ ddbookstoreadmin/app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+  before_action :authenticate_user
+  def authenticate_user
+    authenticate_user =User.find_by_id(session[:current_user_id])
+  end
+  def index
+    render html: "Book Store"
+  end
+end
